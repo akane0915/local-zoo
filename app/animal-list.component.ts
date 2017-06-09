67e7f0442656f0b9acb6f9bde695509228103a8b
akane0915 <asialkane@gmail.com>
2017-06-09 11:15:26 -0700

    add functionality to show details for each animal upon click

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -1,15 +1,40 @@
-import { Component, Input}  from '@angular/core';
-import { Animal }       from './animal.model';
+import { Component, Input}  from "@angular/core";
+import { Animal }       from "./animal.model";
 
 @Component({
-  selector: 'animal-list',
+  selector: "animal-list",
   template: `
-  <ul>
-      <li *ngFor="let animal of childAnimalList">{{animal.name}}</li>
-  </ul>
+  <div class="row">
+    <div class="col-md-6">
+      <h3>Click on an animal to see its details.</h3>
+      <ul class="animal-list">
+          <li *ngFor="let animal of childAnimalList">
+            <a (click)="showDetails(animal)">{{animal.name}}</a>, the {{animal.species}}
+          </li>
+      </ul>
+    </div>
+    <div class="col-md-6">
+      <div *ngIf="animalToBeShown">
+        <h3>{{animalToBeShown.name}}</h3>
+        <p>Species: {{animalToBeShown.species}}</p>
+        <p>Age: {{animalToBeShown.age}}</p>
+        <p>Diet: {{animalToBeShown.diet}}</p>
+        <p>Location in Zoo: {{animalToBeShown.location}}</p>
+        <p>Caretakers: {{animalToBeShown.caretakers}}</p>
+        <p>Sex: {{animalToBeShown.sex}}</p>
+        <p>Likes: {{animalToBeShown.likes}}</p>
+        <p>Dislikes: {{animalToBeShown.dislikes}}</p>
+      </div>
+    </div>
+  </div>
   `
 })
 
 export class AnimalListComponent {
   @Input() childAnimalList: Animal[];
+  animalToBeShown: Animal = null;
+
+  showDetails(animal: Animal) {
+    this.animalToBeShown = animal;
+  }
 }
